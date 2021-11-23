d62ed93f1104aea3dcd8e38e75d73159461eb815
Slava Pinchuk <development1810@gmail.com>
2021-11-23 19:05:18 +0200

    go mod

--- L8/cmd/main.go
+++ L8/cmd/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"HighloadSoftwareArchitecture/L8/app"
+)
+
+func main() {
+	if err := app.Run(); err != nil {
+		log.Fatalf("Run(). Error: '%v'", err)
+	}
+}
